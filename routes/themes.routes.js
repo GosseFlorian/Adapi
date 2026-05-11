a57d800ca4fea7c081e4b080eb3f98a3c3c92577
GosseFlorian <floriangosse91@gmail.com>
2026-05-11 15:41:01 +0200

    ajout creation theme (POST, router + controller)

--- routes/themes.routes.js
+++ routes/themes.routes.js
@@ -2,11 +2,13 @@ import { Router } from "express";
 import {
   getAllThemes,
   getThemeById,
+  createTheme,
 } from "../controllers/themes.controller.js";
 
 const router = Router();
 
 router.get("/", getAllThemes);
 router.get("/:id", getThemeById);
+router.post("/", createTheme);
 
 export default router;
