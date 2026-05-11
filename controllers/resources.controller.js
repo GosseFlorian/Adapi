f3f862ea3c24424c18142f77db702b799024da59
GosseFlorian <floriangosse91@gmail.com>
2026-05-11 16:00:51 +0200

    ajout de update et delete resource en fonction de l'id (routes + controller)

--- controllers/resources.controller.js
+++ controllers/resources.controller.js
@@ -27,3 +27,50 @@ export async function getResourceById(req, res) {
     res.status(500).json({ error: "Erreur serveur" });
   }
 }
+
+// TODO: faire le POST avec check pour le type (enum)
+
+export async function updateResource(req, res) {
+  try {
+    const { id } = req.params;
+    const { title } = req.body;
+
+    if (!title || typeof title !== "string") {
+      return res
+        .status(400)
+        .json({ error: "Le champ resources est requis (string)" });
+    }
+
+    const result = await pool.query(
+      "UPDATE resources SET title = $1 WHERE id = $2 RETURNING *",
+      [title, id],
+    );
+
+    if (result.rowCount === 0) {
+      return res.status(404).json({ error: "Resource introuvable" });
+    }
+
+    res.json(result.rows[0]);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Erreur serveur" });
+  }
+}
+
+export async function deleteResource(req, res) {
+  try {
+    const { id } = req.params;
+    const result = await pool.query("DELETE FROM resources WHERE id = $1", [
+      id,
+    ]);
+
+    if (result.rowCount === 0) {
+      return res.status(404).json({ error: "Resource introuvable" });
+    }
+
+    res.status(204).send();
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Erreur serveur" });
+  }
+}
--- routes/resources.routes.js
+++ routes/resources.routes.js
@@ -1,12 +1,16 @@
 import { Router } from "express";
 import {
+  deleteResource,
   getAllResources,
   getResourceById,
+  updateResource,
 } from "../controllers/resources.controller.js";
 
 const router = Router();
 
 router.get("/", getAllResources);
 router.get("/:id", getResourceById);
+router.patch("/:id", updateResource);
+router.delete("/:id", deleteResource);
 
 export default router;
